a7b379b672cbcd332d0d96000fa28f583b817e0c
javiermixture17 <javiermf.98@gmail.com>
2019-11-03 20:26:21 +0100

    Added test (ca_eu, it_eo)

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,9 +20,21 @@ public class TranslatorServiceTest {
   TranslatorService translatorService;
 
   @Test
-  public void translateTest() {
+  public void translateTest_en_es() {
     TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
     assertEquals("Esto es una prueba de servicio de traducción", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateTest_ca_eu() {
+    TranslatedText translatedText = translatorService.translate("ca", "eu", "Jo visc i estudi a Saragossa");
+    assertEquals("Bizi naiz eta azterketa Zaragoza", translatedText.getTranslation());
+  }
+
+  @Test
+  public void translateTest_it_eo() {
+    TranslatedText translatedText = translatorService.translate("it", "eo", "Verifica se traduci bene il testo");
+    assertEquals("Kontrolu se traduki bone en teksto", translatedText.getTranslation());
   }
 
 }
